07103b2f70396b4177d5e6c0e35586ca733fff5f
YingXue <kalian1127@gmail.com>
2025-05-31 21:50:14 +0000

    basic functional requirements done

--- auto_complete/src/App.tsx
+++ auto_complete/src/App.tsx
@@ -2,7 +2,8 @@ import {AutoComplete} from './components/autoComplete'
 
 function App() {
   return (
-    <div style={{display: 'flex', flexDirection:'column', width: '100%', height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{display: 'flex', flexDirection:'column', width: '100%', height: '100vh', alignItems: 'center'}}>
+      <h1>Auto complete</h1>
       <AutoComplete/>
     </div>
   )
@@ -14,11 +15,12 @@ export default App
 // a input box for user to start typing
 // the auto complete suggestions are rendered in a dropdown (most frequent or with infinite scroll)
 // user typing triggers backend api fetch to grab auto complete suggestions
-// when user select an entry from the suggestions (enter or mouse click), dropdown disappears and input box gets updated
+// when user select an entry from the suggestions, dropdown disappears and input box gets updated
 // input debounced
 
 // accessible:
-// arrow up down
+// arrow up down for suggestion selection
+// can hit enter to auto complete
 // esc to close dropdown
 // aria label
 // color contrast
--- auto_complete/src/components/autoComplete.tsx
+++ auto_complete/src/components/autoComplete.tsx
@@ -1,26 +1,70 @@
 import * as React from 'react';
 import './autoComplete.css'
 
+const useDebouncer = (value: any, delay: number) => {
+    const [debouncedValue, setDebouncedValue] = React.useState(value);
+
+    React.useEffect(()=> {
+        const timerId = setTimeout(()=> {
+            setDebouncedValue(value)
+        }, delay);
+        return () => {clearTimeout(timerId)}
+    }, [value, delay]);
+
+    return debouncedValue;
+}
 export const AutoComplete:React.FC = () => {
     const [userInput, setUserInput] = React.useState('');
-    const [suggestions, setSuggestions] = React.useState(['123', ['12345']]);
+    const [suggestions, setSuggestions] = React.useState([]);
+    const debouncedInput = useDebouncer(userInput, 500);
+
+    React.useEffect(() => {
+        const fetchSuggestions = async () => {
+            if (debouncedInput) {
+                try{
+                    const response = await fetch(`https://dummyjson.com/products/search?q=${debouncedInput.toLocaleLowerCase()}`);
+                    const {products} = await response.json();
+                    const fetchedSuggestions = products.map((product: any) => product.title);
+
+                    // if suggestions has only one entry and it's exactly the same as the input, skip it
+                    if (!(fetchedSuggestions.length === 1 && fetchedSuggestions[0].toLowerCase() === debouncedInput.toLowerCase())) {
+                        setSuggestions(fetchedSuggestions);
+                    } else {
+                        setSuggestions([]);
+                    }
+                }
+                catch {
+                    // do some error handling
+                }
+            }
+            else {
+                setSuggestions([]);
+            }
+        }
+        fetchSuggestions();
+    }, [debouncedInput]);
 
     const onChangeInput = (e: any) => {
         setUserInput(e.target.value);
     }
 
-    React.useEffect(() => {}, [userInput]);
+    const onSuggestionSelected = (suggestion:string) => {
+        setUserInput(suggestion);
+        setSuggestions([]);
+    }
 
     return (
         <>
             <input type='text' value={userInput} onChange={onChangeInput} style={{width: 500}}/>
-            <div className='searchResults'>
-                {suggestions?.map((suggestion, index) => 
-                    <div className='searchResult'>
-                    <span key={index}>{suggestion}</span>
+            {!!suggestions && suggestions.length > 0 && 
+                <div className='searchResults'>
+                {suggestions.map((suggestion, index) => 
+                    <div key={index} className='searchResult'>
+                    <button onClick={() => onSuggestionSelected(suggestion)}>{suggestion}</button>
                     </div>
                 )}
-            </div>
+                </div>
+            }
         </>
     );
 }
