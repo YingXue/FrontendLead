4d38ad63307b4ec081856662c0d2fa6b5136e797
YingXue <kalian1127@gmail.com>
2025-05-31 00:46:10 +0000

    move the container instead of each image

--- image_slider/src/components/imageSlider.jsx
+++ image_slider/src/components/imageSlider.jsx
@@ -25,20 +25,38 @@ export const ImageSlider = () => {
         else setImageIndex(imageIndex+1);
     }
 
+    // outer div container with 100%
+    // inner div with width equal to the number of images, say 5 images, then width is 500%, but the view port is 100% with overflow hidden
+    // then each image takes 20% of the inner width, still 100% of the outer width
+    // the button moves the inner div to the left or right by 20% of the inner width, which is 100% of the outer width according to the imageIndex
     return (
-        <div style={{width: '100%', height: '100%', position: 'relative'}}>
-            <div style={{width: '100%', height: '100%', display: 'flex', overflow: 'hidden'}}>
-                {images.map((image, index) => 
+        <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+            <div
+                style={{
+                    display: 'flex', 
+                    width: `${images.length * 100}%`,
+                    translate: `-${imageIndex* (100/images.length)}%`,
+                    transition: 'translate 0.5s ease-in-out',
+                }}
+            >
+                {images.map((image, index) => (
                     <img
                         key={index}
                         src={image}
-                        alt={'product image '}
-                        className="img-slider-img"
-                        style={{transform: `translateX(-${100 * imageIndex}%)`}}
-                    />)}
+                        alt="product"
+                        style={{
+                            width: `${100 / images.length}%`,   
+                        }}
+                    />
+                ))}
             </div>
-            <button className="img-slider-button" style={{left: 0}} onClick={showPre}><ArrowBigLeft/></button>
-            <button className="img-slider-button"style={{right: 0}} onClick={showNext}><ArrowBigRight/></button>
+
+            <button className="img-slider-button" style={{ left: 0 }} onClick={showPre}>
+                <ArrowBigLeft />
+            </button>
+            <button className="img-slider-button" style={{ right: 0 }} onClick={showNext}>
+                <ArrowBigRight />
+            </button>
         </div>
     );
 }
