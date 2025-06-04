35fe8c911de1c1858328df942a1c75353c99af07
YingXue <kalian1127@gmail.com>
2025-06-03 20:57:36 -0700

    Update App.tsx

--- auto_complete/src/App.tsx
+++ auto_complete/src/App.tsx
@@ -93,16 +93,17 @@ function App() {
 export default App
 
 // functional requirement:
-// a input box for user to start typing
+// an input box for user to start typing
 // the auto complete suggestions are rendered in a dropdown (most frequent or with scroll)
 // user typing triggers backend api fetch to grab auto complete suggestions
 // when user select an entry from the suggestions, dropdown disappears and input box gets updated
-// input debounced
 
 // nice to have:
+// input debounced
+// dropdown only shows when input is in focus
+// caching
 // highlighted the matching input to show relevance
 // api cancellation if new input appears
-// caching
 
 // accessible:
 // arrow up down for suggestion selection
